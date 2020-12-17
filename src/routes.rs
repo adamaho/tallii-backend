af0055b08f21d1fa8f289c85b0a8eeea867125d2
adamaho <aaho.public@gmail.com>
2020-12-16 21:28:25 -0500

    working on event members

--- src/routes.rs
+++ src/routes.rs
@@ -7,6 +7,7 @@ use crate::services::events::routes::{events_entity_routes, events_me_routes, ev
 // };
 use crate::services::friends::routes::{users_me_followers_routes, users_me_following_routes, users_me_follow_username_routes, users_me_following_username_routes, users_username_followers_routes, users_username_following_routes};
 use crate::services::users::routes::{invite_codes_entity_routes, invite_codes_routes, users_email_check_routes, users_login, users_me_routes, users_signup, users_username_routes, users_username_check_routes};
+use crate::services::events::members::routes::event_members_routes;
 
 pub fn define_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(invite_codes_routes())
@@ -27,7 +28,9 @@ pub fn define_routes(cfg: &mut web::ServiceConfig) {
         .service(events_routes())
         .service(events_me_routes())
         .service(events_entity_routes())
-        .service(users_me_events_invites_routes());
+        .service(users_me_events_invites_routes())
+        .service(event_members_routes());
+
         // .service(players_routes())
         // .service(players_entity_routes())
         // .service(teams_routes())
--- src/services/events/members/routes.rs
+++ src/services/events/members/routes.rs
@@ -3,7 +3,7 @@ use actix_web::{web, Resource};
 use super::handlers;
 
 /// Resource routes for event members
-pub fn members_routes() -> Resource {
+pub fn event_members_routes() -> Resource {
     web::resource("/events/{event_id}/members").route(web::get().to(handlers::get_members))
 }
 
@@ -13,8 +13,3 @@ pub fn members_user_routes() -> Resource {
         .route(web::put().to(handlers::update_member))
         .route(web::delete().to(handlers::delete_member))
 }
-
-// /// Resource routes for a specific event player
-// pub fn players_entity_routes() -> Resource {
-//     web::resource("/players/{player_id}").route(web::put().to(handlers::update_player))
-// }
